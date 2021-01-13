1839210ca41fed14567f42b3deab07464bb05efb
Agata Martyna <agatamartynachalupka@gmail.com>
2021-01-13 20:40:57 +0100

    improve forms

--- todo_sql/forms.py
+++ todo_sql/forms.py
@@ -1,10 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, TextAreaField, SelectField
+from wtforms import StringField, TextAreaField, SelectField
 from wtforms.validators import DataRequired
 
 
 class TodoForm(FlaskForm):
-    id = IntegerField("id", validators=[DataRequired()])
+    id = StringField("id")
     nazwa = StringField("nazwa", validators=[DataRequired()])
     deadline = TextAreaField("deadline", validators=[DataRequired()])
-    priorytet = SelectField("priorytet", choices=['bardzo ważne', 'ważne', 'śrenioważne', 'opcjonalne'])
+    priorytet = SelectField(
+        "priorytet",
+        choices=['bardzo ważne', 'ważne', 'śrenioważne', 'opcjonalne'])
+
